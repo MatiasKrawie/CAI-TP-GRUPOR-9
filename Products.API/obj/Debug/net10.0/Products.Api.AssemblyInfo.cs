f91d418eb11093d63cc7c23a62dd661855f9cdfa
Mati-Kraw <krawieckimatias@gmail.com>
2026-05-26 12:39:34 -0300

    Renombrado físico de carpeta a Products.API

--- Products.API/obj/Debug/net10.0/Products.Api.AssemblyInfo.cs
+++ Products.API/obj/Debug/net10.0/Products.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Products.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b268b85c437f34b3bb971075580a8a7285183cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Products.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Products.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
