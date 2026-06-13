a35e900833b864269184b92b5ff048c67e04b2d5
OwenGribov <owen.grib@gmail.com>
2026-06-12 22:45:19 -0300

    chau sales

--- Sales.Api/Sale.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sales.Api;
-
-public class Sale
-{
-    public int Id { get; set; }
-    public int ProductId { get; set; }
-    public int Quantity { get; set; }
-    public DateTime SaleDate { get; set; } = DateTime.Now;
-}
